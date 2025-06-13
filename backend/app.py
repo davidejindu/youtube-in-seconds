d13f6a081c6a4486cd00440a8d746d035c0e5e8d
David Ejindu <ejindudavid540@gmail.com>
2025-06-13 00:10:30 -0400

    finished backend implementation

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,10 @@ from youtube_transcript_api import YouTubeTranscriptApi
 import os
 from dotenv import load_dotenv
 import re
+import google.generativeai as genai
 
 load_dotenv()
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 app = Flask(__name__)
 CORS(app)
@@ -23,13 +25,19 @@ def summary():
         yt_transcript = YouTubeTranscriptApi.get_transcript(yt_id)
     
         complete_transcript = " ".join(item["text"] for item in yt_transcript)
-        summary = f"(Mock summary): {complete_transcript[:300]}..."
+        summary = gemini_summary(complete_transcript)
         
         return jsonify({"summary": summary})
     
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     
+def gemini_summary(transcipt):
+    prompt = ("Summarize this Youtube transcipt into clear concise paragraphs on the main points.\n\n"f"{transcipt}")
+    
+    model = genai.GenerativeModel('models/gemini-1.5-flash')
+    response = model.generate_content(prompt)
+    return response.text.strip()
     
 def get_yt_id(url):
     found = re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11})", url)
