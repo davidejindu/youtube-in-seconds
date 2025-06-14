559037e4565ba25687cc660133f3a1e850a0b262
David Ejindu <ejindudavid540@gmail.com>
2025-06-14 13:20:03 -0400

    hardcode render link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/summary", {
+      const response = await fetch("https://youtube-in-seconds.onrender.com/summary", {
         method: "POST",
         headers: {"Content-Type" : "application/json"},
         body: JSON.stringify({url: ytUrl})
